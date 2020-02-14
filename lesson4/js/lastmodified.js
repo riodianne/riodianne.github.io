6d7d11d1f559b190228e187aaae90cfcf0ad9c04
riodianne <dianamay.miranda@gmail.com>
2020-02-14 17:50:36 +0800

    first commit

--- lesson4/js/lastmodified.js
+++ lesson4/js/lastmodified.js
@@ -1,2 +1,6 @@
-let oLastModif = document.lastModified;
+var d = new Date(document.lastModified);
+
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let oLastModif = days[d.getDay()] + ", " + d.getDay() + " " + months[d.getMonth()] + " " + d.getFullYear();
 document.getElementById("lastModified").innerHTML="Last Updated: " + oLastModif;
