f7e2c3966bf39f360ca1296b3c86eab27ce14c4e
riodianne <dianamay.miranda@gmail.com>
2020-03-14 20:48:15 +0800

    Lesson 10

---
+++ lesson-10/js/weatherforecast.js
@@ -0,0 +1,32 @@
+var dd = new Date();
+// console.log(d.getDay());
+
+var dcurrentday=dd.getDay();
+
+var daycheck = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+
+var label=document.getElementsByClassName('label');
+
+
+//dcurrentday =0; //this is for test
+//get currrent day
+//loop to 5 day forecast
+//if the current day exceed to 6 it goes to 1 which is sunday
+
+for(var x=0; x<5; x++){
+
+    if(dcurrentday<=6){
+        // console.log(daycheck[dcurrentday]);
+       label[x].innerHTML=daycheck[dcurrentday];
+        dcurrentday++;
+    }
+    else{
+        dcurrentday=0;
+        // console.log(daycheck[dcurrentday]);
+        label[x].innerHTML=daycheck[dcurrentday];
+        dcurrentday++;
+    }
+    
+
+}
---
+++ lesson-10/js/windchill.js
@@ -0,0 +1,18 @@
+var f;
+var t=parseInt(document.getElementById("high").innerText);
+var s=parseInt(document.getElementById("windspeed").innerText);
+
+
+//Wind Chill Temperature is officially defined for temperatures at or
+// below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph).
+
+if(t<=50 && s>=3){
+//f= 35.74 + 0.6215t - 35.75 s 0.16 + 0.4275t s 0.16
+f=35.74+0.6215*t-35.75*Math.pow(s,0.16) + 0.4275*t*Math.pow(s,0.16);
+f=Math.round(f*100)/100;
+document.getElementById("windchill").innerHTML=f;
+}
+
+else{
+    document.getElementById("windchill").innerHTML="N/A";
+}
